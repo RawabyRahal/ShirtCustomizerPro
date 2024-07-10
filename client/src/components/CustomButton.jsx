3c93117538ddd76c02f6597a29a408d7f0b7e5e6
RawabyRahal <rawaby.16.4@gmail.com>
2024-07-11 01:50:25 +0300

    implemented basic layout for Customizer page

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,19 +1,19 @@
 import { color } from "framer-motion";
 import React from "react";
-import state from '../store';
+import state from "../store";
 import { useSnapshot } from "valtio";
-import { getContrastingColor } from '../config/helpers';
+import { getContrastingColor } from "../config/helpers";
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
-    const snap = useSnapshot(state);
-    const generateStyle = (type) => {
-        if (type === "filled") {
-            return{
-                backgroundColor: snap.color,
-                color: "#fff",
-            }
-        }
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: "#fff",
+      };
     }
+  };
   return (
     <button
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
